5955d870c4fb101d7147675145b73242af9b3893
Spencer Lepine <spencerlepine26@gmail.com>
2022-07-12 07:24:57 -0700

    use CORS for server

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const config = require('./config')
+const cors = require("cors")
 
 const routes = require("./src/routes")
 const app = express()
 
 const jsonParser = bodyParser.json()
 app.use(jsonParser)
+app.use(cors)
+
 app.use(routes)
 
 module.exports = app
